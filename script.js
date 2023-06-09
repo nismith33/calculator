c4d0b3c76c5f7cb13934cf8e4c9a27f69d4f1bb0
nismith33 <nismith33@gmail.com>
2023-06-09 13:01:36 -0400

    Add function to pass values to correct operation function

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
+let firstValue;
+let secondValue;
+let operator;
+
 function add(a,b) {
-    return a+b;
+    return `${(+a)+(+b)}`;
 }
 
 function subtract(a,b) {
-    return a-b;
+    return `${(+a)-(+b)}`;
 }
 
 function divide(a,b) {
-    if (b===0) {
+    if (+b===0) {
         alert("Don't do that")
         return "error"
     }
     else {
-        return a/b;
+        return `${(+a)/(+b)}`;
     }
 }
 
 function multiply(a,b) {
-    return a*b;
+    return `${(+a)*(+b)}`;
+}
+
+function operate(operator, firstValue, secondValue) {
+    if (operator==="+") {
+        return add(firstValue,secondValue);
+    }
+    else if (operator==="-") {
+        return subtract(firstValue,secondValue);
+    }
+    else if (operator==="*") {
+        return multiply(firstValue,secondValue);
+    }
+    else if (operator==="/") {
+        return divide(firstValue,secondValue);
+    }
 }
